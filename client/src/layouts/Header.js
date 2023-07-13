493ab65fba942317478bd22db7a44e4f0ca84ac7
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-13 17:04:21 +0900

    Header구성 및 SignIn,Up Modal 작성

--- client/src/layouts/Header.js
+++ client/src/layouts/Header.js
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Nav, Navbar, Button } from "react-bootstrap";
+import SignUpModal from "../modals/SignUpModal";
+import SignInModal from "../modals/SignInModal";
 
 const Header = () => {
+  const [signUpModalOn, setSignUpModalOn] = useState(false);
+  const [signInModalOn, setSignInModalOn] = useState(false);
   return (
-    <header>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand>MERN Login Form</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link>
-                <Button variant="primary">Sign In</Button>
-              </Nav.Link>
-              <Nav.Link>
-                <Button variant="secondary">Sign Up</Button>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
+    <>
+      <SignUpModal show={signUpModalOn} onHide={() => setSignUpModalOn(false)} />
+      <SignInModal show={signInModalOn} onHide={() => setSignInModalOn(false)} />
+      <header>
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand>MERN Login Form</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link>
+                  <Button variant="primary" onClick={() => setSignInModalOn(true)}>
+                    Sign In
+                  </Button>
+                </Nav.Link>
+                <Nav.Link>
+                  <Button variant="secondary" onClick={() => setSignUpModalOn(true)}>
+                    Sign Up
+                  </Button>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    </>
   );
 };
 
