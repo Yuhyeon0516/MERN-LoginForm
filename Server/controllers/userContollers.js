681f847be628613e15734ed377354143947a712a
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-13 17:51:27 +0900

    express 및 server구성의 대략적인 구조 생성

---
+++ Server/controllers/userContollers.js
@@ -0,0 +1,7 @@
+export const userSignInController = (req, res) => {
+  // SignIn post 내용 작성
+};
+
+export const userSignUpController = (req, res) => {
+  // SignUp post 내용 작성
+};
--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,18 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import express from "express";
+import userRoutes from "./routes/userRoutes.js";
 
-const mongoose = require("mongoose");
+const server = express();
+server.use(express.json());
+server.use("/api/user", userRoutes);
+
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URL)
-  .then(() => console.log("Mongodb Conneted"))
+  .then(() => {
+    console.log("Mongodb Conneted");
+    server.listen(4000, () => console.log("Express Server Start"));
+  })
   .catch((err) => console.log("ERR: ", err));
