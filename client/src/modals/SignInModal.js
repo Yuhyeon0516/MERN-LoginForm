77e1cdcd1dd57f169ff843b9e8e5c89168b39e43
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-17 14:05:03 +0900

    SignIn, SignUp 값 받아올 준비 완료

--- client/src/modals/SignInModal.js
+++ client/src/modals/SignInModal.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, Modal, Container } from "react-bootstrap";
 
 const SignInModal = ({ show, onHide }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (e) => {
+    const value = e.target.value;
+    if (e.target.id === "email") {
+      setEmail(value);
+    } else if (e.target.id === "password") {
+      setPassword(value);
+    } else {
+      console.log("Not a target");
+    }
+  };
+
   return (
     <Modal show={show} onHide={onHide} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
       <Container>
@@ -12,12 +26,12 @@ const SignInModal = ({ show, onHide }) => {
           <Form>
             <Form.Group className="mb-3">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control id="email" value={email} onChange={onChange} type="email" placeholder="Enter email" />
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control id="password" value={password} onChange={onChange} type="password" placeholder="Password" />
             </Form.Group>
             <div className="d-grid gap-2">
               <Button size="lg" variant="info" type="button" className="my-3">
